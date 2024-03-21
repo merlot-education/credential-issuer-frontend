30c2440f00764715279e01bc78249f77399b4bb6
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-03-21 09:07:49 +0100

    switch ocm urls to cluster

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,28 @@
 export const environment = {
   production: false,
   orgasAPI: 'https://api.dev.merlot-education.eu/organisations/?page=0&size=50',
-  //invitationUrlApi: 'http://213.165.77.158:3003/v1/invitation-url?alias=trust',
-  invitationUrlApi: 'https://secure-routes.jumpy.dev/invitation-url.php',
+  // invitationUrlApi: 'http://213.165.77.158:3003/v1/invitation-url?alias=trust',
+  // invitationUrlApi: 'https://secure-routes.jumpy.dev/invitation-url.php',
   // invitationUrlApi:
   //   'https://ocm.dev.merlot-education.eu/connection/v1/invitation-url?alias=trust',
-  // invitationUrlApi:
-  //   'https://ocm.dev.merlot-education.eu/connection/v1/invitation-url?alias=trust',
-  //connectionApi: 'http://213.165.77.158:3003/v1/connections',
-  connectionApi: 'https://secure-routes.jumpy.dev/connections.php',
+  invitationUrlApi:
+    'https://ocm.dev.merlot-education.eu/connection/v1/invitation-url?alias=trust',
+  // connectionApi: 'http://213.165.77.158:3003/v1/connections',
+  // connectionApi: 'https://secure-routes.jumpy.dev/connections.php',
   // connectionApi:
   //   'https://ocm.dev.merlot-education.eu/connection/v1/connections',
-  // connectionApi:
-  //   'https://ocm.dev.merlot-education.eu/connection/v1/connections',
-  //createCredentialApi: 'http://213.165.77.158:3005/v1/create-offer-credential',
-  createCredentialApi:
-    'https://secure-routes.jumpy.dev/create-offer-credential.php',
+  connectionApi:
+    'https://ocm.dev.merlot-education.eu/connection/v1/connections',
+  // createCredentialApi: 'http://213.165.77.158:3005/v1/create-offer-credential',
   // createCredentialApi:
-  //   'https://ocm.dev.merlot-education.eu/attestation/v1/create-offer-credential',
+  //   'https://secure-routes.jumpy.dev/create-offer-credential.php',
+  createCredentialApi:
+    'https://ocm.dev.merlot-education.eu/attestation/v1/create-offer-credential',
   // 'https://ocm.dev.merlot-education.eu/credential/v1/create-offer-credential',
-  //credentialApi: 'http://213.165.77.158:3005/v1/credential',
-  credentialApi: 'https://secure-routes.jumpy.dev/credential.php',
-  // credentialApi:
-  //   'https://ocm.dev.merlot-education.eu/attestation/v1/credential',
+  // credentialApi: 'http://213.165.77.158:3005/v1/credential',
+  // credentialApi: 'https://secure-routes.jumpy.dev/credential.php',
+  credentialApi:
+    'https://ocm.dev.merlot-education.eu/attestation/v1/credential',
   // credentialDefinitionId:
   //   '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:90163:MerlotFederationLoginLongerWith240',
   credentialDefinitionId:
