1659f0b9ce2acefb989cf3cbdfce14b666c12338
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-06-20 17:23:28 +0200

    update id

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -26,5 +26,5 @@ export const environment = {
   // credentialDefinitionId:
   //   '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:90163:MerlotFederationLoginLongerWith240',
   credentialDefinitionId:
-    'UaT2udeT53Tr1n6itV46Z:3:CL:988170:MerlotLoginDemoCredDef',
+    'UaT2udeT53Tr1n6itV46Z:3:CL:988172:MerlotLoginDemo1CredDef',
 };
