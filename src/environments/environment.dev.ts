d39d0b2de41f6c70b5f299798e84e0ed09b3f6ab
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-07-08 10:18:09 +0200

    update credef

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -26,5 +26,5 @@ export const environment = {
   // credentialDefinitionId:
   //   '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:90163:MerlotFederationLoginLongerWith240',
   credentialDefinitionId:
-    'K8j8nFTijJTCsFwrRNE3Df:3:CL:1047693:MerlotLoginDev080724CredDef',
+    'K8j8nFTijJTCsFwrRNE3Df:3:CL:1047693:MerlotLoginDev080724CredDef-2',
 };
