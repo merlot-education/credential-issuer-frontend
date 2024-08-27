ee1d15d295e3a45fc15e39f09d899f5382bad7e7
mircea-saracin <158305189+mircea-saracin@users.noreply.github.com>
2024-08-27 09:45:54 +0300

    Update environment.dev.ts
    Modified CredDef ID

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -42,5 +42,5 @@ export const environment = {
   // credentialDefinitionId:
   //   '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:90163:MerlotFederationLoginLongerWith240',
   credentialDefinitionId:
-    'K8j8nFTijJTCsFwrRNE3Df:3:CL:1047693:MerlotLoginDev19824CredDef',
+    'K8j8nFTijJTCsFwrRNE3Df:3:CL:1047693:MerlotLoginDev270824CredDef',
 };
