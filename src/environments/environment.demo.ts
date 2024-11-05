2691c8370e82f911b855d04542066e243f9b72f5
sesoman <158305189+sesoman@users.noreply.github.com>
2024-11-05 14:41:21 +0200

    Update environment.demo.ts

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -42,5 +42,5 @@ export const environment = {
   // credentialDefinitionId:
   //   '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:90163:MerlotFederationLoginLongerWith240',
   credentialDefinitionId:
-    'UaT2udeT53Tr1n6itV46Z:3:CL:1047695:MerlotLoginDemo281024CredDef',
+    'UaT2udeT53Tr1n6itV46Z:3:CL:1047695:MerlotLoginDemo5112024CredDef',
 };
